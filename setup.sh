6e71b70cfe691cc87ffaeae0685daf144389554c
Ark-Arch <69996990+Ark-Arch@users.noreply.github.com>
2024-05-25 10:05:30 +0000

    edit the setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Setup Python environment
-python3 -m venv venv
-source venv/bin/activate
+#python3 -m venv venv
+#source venv/bin/activate
 
 # Install system dependencies
 sudo apt-get update
@@ -13,10 +13,11 @@ pip install tensorflow
 pip install tensorflow_datasets
 pip install scikit-image
 pip install mltu
-pip install stow
-pip install tf2onnx
-pip install pydot graphviz
 pip install scikit-learn
+
+#pip install stow
+#pip install tf2onnx
+#pip install pydot graphviz
 
 # Clear terminal and show a message
 clear
